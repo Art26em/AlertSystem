9ed6f07d43678ea6bb4153be140ac0f1b0e32926
Artem Timonin <t1moninartyom@yandex.ru>
2025-07-04 08:14:39 +0500

    Removed unnecessary comment

--- Assets/Scripts/Basic/RobberController.cs
+++ Assets/Scripts/Basic/RobberController.cs
@@ -9,7 +9,6 @@ public class RobberController : MonoBehaviour
     
     private Animator _animator;
     
-    // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
